4ede35bc7e4cd8293c2ce236f88a196684909d3e
Olesia Miller <Olesia_Miller@epam.com>
2021-06-16 00:29:42 +0300

    add vikings two page site

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const menuBtn = document.querySelector('.menu__btn');
+const menuList = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  menuList.classList.toggle('menu__list--active');
+});
+
+$(function(){
+  $('[data-fancybox]').fancybox({
+    youtube : {
+      controls : 0,
+      showinfo : 0
+    },
+  });
+
+  $('.heroes__slider-img').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.heroes__slider-text',
+    prevArrow: '<button class="slick-btn slick-prev"><img src="../images/arrow-left.png" alt="prev"></button>',
+    nextArrow: '<button class="slick-btn slick-next"><img src="../images/arrow-right.png" alt="next"></button>',
+  });
+  $('.heroes__slider-text').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.heroes__slider-img',
+    fade: true,
+    arrows: false,
+  });
+});
